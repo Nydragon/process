1df9abc0e664d320dac48d7cd6df7c0e9ef595cc
Nydragon <6582485n@proton.me>
2023-11-25 19:21:41 +0900

    feat(process): add struct keys

--- src/process.rs
+++ src/process.rs
@@ -5,8 +5,42 @@ use crate::parser::{DataError, Parser};
 pub type Processes = Vec<Process>;
 
 #[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct Process {}
+enum State {
+    /// Sleeping
+    S = 1,
+    /// Idle
+    I = 2,
+    /// Running
+    R = 3,
+    ///  Disk Sleep
+    D = 4,
+    /// Zombie (waiting for parent to read its exit status)
+    Z = 5,
+    /// traced or suspended (e.g by SIGTSTP)
+    T = 6,
+    /// Paging
+    W = 7,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Process {
+    /// Process ID
+    pid: u32,
+    /// Parent Process ID
+    ppid: u32,
+    /// User
+    user: String,
+    priority: u8,
+    niceness: u8,
+    virt: u32,
+    res: u32,
+    shr: u32,
+    s: State,
+    cpu_abs: u32,
+    mem_abs: u32,
+    time: u32,
+    command: String,
+}
 
 /// Implementing Parser for [Processes] instead of [Process].
 impl Parser for Processes {
